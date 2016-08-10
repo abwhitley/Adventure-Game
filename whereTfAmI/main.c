2e831e3438c32710fa78dceab4f7ce9b675d5bc1
abwhitley <a.blakewhitley@gmail.com>
2016-08-09 23:51:58 -0400

    Finished without retry

--- whereTfAmI/main.c
+++ whereTfAmI/main.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+int redo = -1;
+int cont = -1;
 int door = -1;
 int room = -1;
 int theEnd(int ending);
@@ -18,11 +20,19 @@ void room3();
 void room4();
 void room5();
 void room6();
-
+void death();
+void fleshWound();
+void onlyAScratch();
+void monty();
+void paddleFaster();
+int retry(int redo);
 
 int main(int argc, const char * argv[]) {
-    
-    printf("Theres door 1 and door 2. What is your choice?\n");
+   // retry(<#int redo#>);
+   // if (redo!= -1 || redo !=1){
+   //     return 0;
+    //}else{
+    printf("You are dropped in a empy room all white with two doors on opposite side.\nDoor one is a blood red.\nDoor two is dark blue.\nWhich do you choose?\n(Enter 1 or 2)\n");
     scanf("%d", &door);
     printf("You choose door number %d\n", door);
     
@@ -35,15 +45,17 @@ int main(int argc, const char * argv[]) {
     }
     
     return 0;
+    //}
+
 }
 
 void room2(){
     printf("You have arrived in room 2\n");
-    printf("Theres door 3 and door 4. Which do you choose?\n");
+    printf("After walking through the red door\nYou notice vents in the floor to small to fit in,\nbut there is also a wooden ladder leading to a trap door in the ceithing\nAnd also a set of stairs leading down a corridor to another red door.\nDo you choose to go down the stairs or up the ladder?\n(Choose 1 for stairs or 2 for ladder)\n");
     scanf("%d", &door);
     printf("You chose door %d\n", door);
     
-    if(door == 3){
+    if(door == 1){
         room4();
     }else{
         room5();
@@ -53,48 +65,93 @@ void room2(){
 }
 
 void room3(){
-    printf("You have arrived in room 3\n");
-    printf("Theres door 5 and door 6. Which do you choose?\n");
+    printf("You walk through the blue door and discover a portal\nIt looks dangerous but the blue door has disappered\nDo you go through the portl or do you sit and die?\n(Enter 1 to sit and die or 2 to be adeventourous)\n");
     scanf("%d", &door);
-    printf("You choose door %d\n", door);
-    
-    if(door == 5){
-        room5();
+    if(door == 1){
+        printf("You chose the cowards way.");
+        death();
     }else{
-        room6();
+        int portal = arc4random_uniform(2);
+        printf("%d", portal);
+        if (portal == 0) {
+            room5();
+        }else{
+            room6();
+        }
+        
     }
 }
 
 void room4(){
-    printf("Welcome to room 4\n");
+    printf("As you walk down the stairs towards the red door\nYou start to hear a faint sound what sounds like a horse galloping.\nYou disregard the sound and continue though the door.\n As you open the door a opeing in a forest appears before you.\n");
+    monty();
 }
 
 void room5(){
-    printf("Welcome to room 5\n");
+    printf("You climb up the ladder, open the trap door\nYou find yourself transported to a river and see a cannoe on the bank\n Do you jump in the cannoe or walk along the bank?\n(Enter 1 to jump in the cannoe or 2 for a walk along the bank)\n");
+    fpurge(stdin);
+    scanf("%d", &cont);
+    if(cont == 1){
+        
+    }
 }
 
 void room6(){
-    printf("Welcome to room 6\n");
+    death();
 }
 
-/*int doorChoice(int door){
-    
-}*/
+void death(){
+    printf("You are dead\n");
+}
+void fleshWound(){
+    printf("Mearly a flesh would - you say\n");
+    printf("The man cuts of your legs and leaves you for death.\n");
+    death();
+}
+void onlyAScratch(){
+    printf("Tis only a scratch ive had worse. - you say\n");
+}
 
-
-/*int theEnd(int ending){
-    switch(ending){
-        case 0: {//ending one
-            
-        }
-        case 1: {//ending two
-            
-        }
-        case 2: {//ending three
-            
-        }
-        defualt: {//mystery ending??
-            
+void monty(){
+    printf("The sound of horses grows louder,\nAnd as fast as you can think a man galloping through the woods\nHe comes around the bend with a hunchback man clapping coconuts together appears.\nHe asks to get by you do you allow him?\n(Enter 1 for yes or 2 for no.)\n");
+    scanf("%d", &door);
+    if(door == 1){
+        printf("Then man invites you on his quest for the holy grail.\n");
+    }else{
+        printf("He challanges you to a fight, and you lose an arm.\n");
+        onlyAScratch();
+        printf("Would you like to continue fighting? Enter 1 for yes 2 for no.\n");
+        fpurge(stdin);
+        scanf("%d", &cont);
+        if(cont == 1){
+            printf("You have lost both arms.\n");
+            fleshWound();
+        }else{
+            printf("He spares your life but your bleed profusly from the 'scratch'.\nThe blood thirst rabbit does not spare you.\n");
+            death();
         }
     }
-}*/
+    
+}
+void paddleFaster(){
+    printf("You here banjos\n");
+    printf("Paddle Faster?\n(Enter 1 for yes or 2 to enjoy the music)\n");
+    fpurge(stdin);
+    scanf("%d", &cont);
+    if (cont == 1) {
+        printf("escaped somthing nasty and made it down the stream without incident.");
+    }else{
+        death();
+    
+    }
+}
+int retry(int redo){
+    printf("Would you like to Start?\nEnter 1 to Start.\n0 to exit.\n");
+    fpurge(stdin);
+    scanf("%d", &redo);
+    return redo;
+    
+}
+
+
+
